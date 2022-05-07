3f0b1dc0a31b691a3ea1ae58f609c18b80402ffb
kirwin201 <kirwin.201@gmail.com>
2022-05-06 23:41:07 -0400

    working on layout

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAu5IS3DjfjS6Z3_MGrYpCVz8RtGv4vIjs",
+  authDomain: "sfl-dynasty.firebaseapp.com",
+  databaseURL: "https://sfl-dynasty-default-rtdb.firebaseio.com",
+  projectId: "sfl-dynasty",
+  storageBucket: "sfl-dynasty.appspot.com",
+  messagingSenderId: "838922834336",
+  appId: "1:838922834336:web:adf0c7d78b4c9b7a7814c5",
+  measurementId: "G-4N2PKVDX31"
+});
+
+const analytics = getAnalytics(firebaseApp);
+
 if (environment.production) {
   enableProdMode();
 }
